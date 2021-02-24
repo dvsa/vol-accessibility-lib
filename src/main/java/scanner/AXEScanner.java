758e33e18bea6cc0db9a601e86a713cb22a8a632
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2021-02-24 15:29:05 +0000

    update axe package

--- src/main/java/scanner/AXEScanner.java
+++ src/main/java/scanner/AXEScanner.java
@@ -2,12 +2,14 @@ package scanner;
 
 import activesupport.IllegalBrowserException;
 import activesupport.driver.Browser;
+import com.deque.html.axecore.axeargs.AxeRuleOptions;
 import com.deque.html.axecore.axeargs.AxeRunOptions;
 import com.deque.html.axecore.results.Results;
 import com.deque.html.axecore.results.Rule;
 import com.deque.html.axecore.selenium.AxeBuilder;
+import com.deque.html.axecore.selenium.AxeReporter;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -16,51 +18,62 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AXEScanner {
 
+    private static final String userDirectory = System.getProperty("user.dir");
+
+    public String axeFindings;
+    public String findingsThatNeedReviewing;
+
     private int totalViolationsCount;
     private int numberOfViolationsFoundPerPage;
 
-    private String[] standards = new String[]{System.getProperty("standards.scan")};
-    private String[] rules = new String[]{System.getProperty("rules.scan")};
-
-    public String[] getStandards() {
-        return standards;
-    }
-
-    public static List<Rule> axeFindings;
-
-    public void setStandards(String[] standards) {
-        this.standards = standards;
-    }
-
-    public String[] getRules() {
-        return rules;
-    }
-
-    public void setRules(String[] rules) {
-        this.rules = rules;
-    }
+    private List<String> tags = Arrays.asList(System.getProperty("standards.scan"));
+    private List<String> rules = Collections.singletonList(System.getProperty("rules.scan"));
 
 
     public int getTotalViolationsCount() {
         return totalViolationsCount;
     }
 
-
     public int getNumberOfViolationsFoundPerPage() {
         return numberOfViolationsFoundPerPage;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public List<String> getRules() {
+        return rules;
+    }
+
+    public void setRules(List<String> rules) {
+        this.rules = rules;
+    }
+
+    public AXEScanner() {
+        if (getTags().get(0) == null) {
+            setTags(Arrays.asList("wcag2a", "wcag412", "wcag143", "wcag2aa", "cat.**"));
+        } else {
+            setTags(tags);
+        }
     }
 
     public void scan() throws IOException, IllegalBrowserException {
         AxeRunOptions runOptions = new AxeRunOptions();
         runOptions.setXPath(true);
-        runOptions.setResultTypes(Collections.singletonList("violations"));
+
+        AxeRuleOptions enabledRules = new AxeRuleOptions();
+        enabledRules.setEnabled(true);
 
         Results axeResponse = new AxeBuilder().withOptions(runOptions)
-                .withTags(Arrays.asList("wcag2aa"))
-                .withRules(Arrays.asList("accesskeys", "bypass", "focus-order-semantics", "region", "skip-link", "tabindex"))
-                .exclude(Collections.singletonList("#global-footer"))
+                .withTags(tags)
                 .analyze(Browser.navigate());
 
+        List<Rule> inapplicable = axeResponse.getInapplicable();
         List<Rule> violations = axeResponse.getViolations();
 
         if (violations.size() == 0) {
@@ -69,6 +82,11 @@ public class AXEScanner {
             numberOfViolationsFoundPerPage = violations.size();
             totalViolationsCount += violations.size();
         }
-        axeFindings = violations;
+
+        List<List<Rule>> multiList = Arrays.asList(violations, inapplicable);
+        for (List<Rule> ruleList : multiList) {
+            AxeReporter.getReadableAxeResults("violations", Browser.navigate(), ruleList);
+        }
+        axeFindings = AxeReporter.getAxeResultString();
     }
 }
--- src/main/java/scanner/ReportGenerator.java
+++ src/main/java/scanner/ReportGenerator.java
@@ -11,8 +11,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.time.Instant;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class ReportGenerator {
@@ -35,13 +34,15 @@ public class ReportGenerator {
     public void violationDetailsReportSection(String scanURL, AXEScanner scanner) throws IOException {
         File violationTemp = new File(userDirectory.concat("/target/violations.txt"));
         Mustache mustache = mf.compile("violations.mustache");
-        Scanner textScanner = new Scanner(String.valueOf(AXEScanner.axeFindings));
+
+        Scanner textScanner = new Scanner(scanner.axeFindings);
         while (textScanner.hasNext()) {
             String violations = String.valueOf((textScanner.nextLine()));
             mustache.execute(new FileWriter(violationTemp, true),
                     new MustacheSettings(violations, null)).flush();
         }
         textScanner.close();
+
         try {
             String details = readLinesAsInputStream(ReportGenerator.class.getClassLoader().getResourceAsStream("violations.html"));
             String detailsRegex = "violations";
@@ -70,7 +71,8 @@ public class ReportGenerator {
             tempDetailsWriter.flush();
             tempDetailsWriter.close();
 
-        } catch (Exception e) {
+        } catch (
+                Exception e) {
             e.printStackTrace();
         }
 
@@ -78,6 +80,7 @@ public class ReportGenerator {
             tempDetailsFile.delete();
             tempCompleteDetailsFile.delete();
         }
+
     }
 
     public void createReport(AXEScanner scanner) throws IOException, URISyntaxException {
