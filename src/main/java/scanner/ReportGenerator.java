b5d01dc38b3bb4142e87c5f7b59bfcf10e123bf7
Stefan Ranoszek <stefan48500@gmail.com>
2024-05-16 14:32:55 +0100

    fix:revert scanner changes

--- src/main/java/scanner/ReportGenerator.java
+++ src/main/java/scanner/ReportGenerator.java
@@ -231,8 +231,7 @@ public class ReportGenerator {
     private static String readLines(String filePath) {
         StringBuilder contentBuilder = new StringBuilder();
         try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
-            final StringBuilder finalContentBuilder = contentBuilder;
-            stream.forEach(s -> finalContentBuilder.append(s).append("\n"));
+            stream.forEach(s -> contentBuilder.append(s).append("\n"));
         } catch (IOException e) {
             e.printStackTrace();
         }
